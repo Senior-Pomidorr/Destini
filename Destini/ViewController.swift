c434a664dc37bf3c7e3e0a774c86a978cde44079
«Daniil <danilkulikovskiy@gmail.com>
2023-04-25 23:42:56 +0300

    Initial Commit

---
+++ Destini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by Daniil Kulikovskiy on 25.04.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
